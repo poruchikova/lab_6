954ee8721e22398fdc6970b4e10acba6430f610b
Kristina Poruchikova <105925710+poruchikova@users.noreply.github.com>
2022-05-21 06:06:02 +0500

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,7 +1,6 @@
+import matplotlib.pyplot as plt
 import seaborn
 from pandas import DataFrame
-import matplotlib.pyplot as plt
-
 
 penguins: DataFrame = seaborn.load_dataset("penguins")
 seaborn.countplot(x="species", data=penguins)
