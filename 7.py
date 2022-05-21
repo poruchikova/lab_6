6679c83bd17bb5de387bd49fec55183e469725f6
Kristina Poruchikova <105925710+poruchikova@users.noreply.github.com>
2022-05-21 06:06:15 +0500

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,8 +1,7 @@
-import seaborn
-from pandas import DataFrame
 import matplotlib.pyplot as plt
 import numpy
-
+import seaborn
+from pandas import DataFrame
 
 planets: DataFrame = seaborn.load_dataset("planets")
 print(planets.groupby(["method", "number"], as_index=False).agg({"value": numpy.mean}))
